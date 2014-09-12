6505939dc6ec0676ad5e7ed6349e7fa249762cee
Data Manager <datamgr@crow.marine.usf.edu>
2014-09-12 15:08:44 +0000

    corrected spacing issue

--- adcp_qartod_qaqc/trdi.py
+++ adcp_qartod_qaqc/trdi.py
@@ -78,8 +78,8 @@ class TRDIQAQC(object):
 
             self.current_speed.append(abs(z))
             direction = math.atan2(z.real, z.imag)*180/math.pi
-                 if direction < 360:
-                    direction = direction + 360
+            if direction < 360:
+                direction = direction + 360
 
             self.current_direction.append(direction)
 
